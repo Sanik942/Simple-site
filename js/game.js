c96d7136dab1467061ad2753e297b70ae6063ffd
Sanik942 <81910746+Sanik942@users.noreply.github.com>
2022-04-29 14:10:58 +0500

    Update game.js

--- js/game.js
+++ js/game.js
@@ -109,12 +109,12 @@ function drawBricks() {
 function drawScore() {
   ctx.font = "16px Arial";
   ctx.fillStyle = "#0095DD";
-  ctx.fillText("Счет: "+score, 8, 20);
+  ctx.fillText("Score: "+score, 8, 20);
 }
 function drawLives() {
   ctx.font = "16px Arial";
   ctx.fillStyle = "#0095DD";
-  ctx.fillText("Жизни: "+lives, canvas.width-65, 20);
+  ctx.fillText("Lives: "+lives, canvas.width-65, 20);
 }
 
 function draw() {
